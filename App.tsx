7d285742539ce4afd66efdd80f7da8fa6d713034
josmtz <josejavmartine26@gmail.com>
2020-04-23 15:08:16 -0700

    Init the project

--- App.tsx
+++ App.tsx
@@ -1,10 +1,27 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { useFonts } from "@use-expo/font";
+import { AppLoading } from "expo";
+
+const CustomFonts = {
+  "NotoSans-Bold": require("./assets/fonts/NotoSans-Bold.ttf"),
+  "NotoSans-BoldItalic": require("./assets/fonts/NotoSans-BoldItalic.ttf"),
+  "NotoSans-Italic": require("./assets/fonts/NotoSans-Italic.ttf"),
+  "NotoSans-Regular": require("./assets/fonts/NotoSans-Regular.ttf"),
+};
 
 export default function App() {
+  const [isLoaded] = useFonts(CustomFonts);
+
+  if (!isLoaded) {
+    return <AppLoading />;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={{ fontFamily: "NotoSans-Bold" }}>
+        Open up App.tsx to start working on your app!
+      </Text>
     </View>
   );
 }
@@ -12,8 +29,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
