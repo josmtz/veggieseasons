b424c9c127a40962ecfb84228a0c7a089d135a35
josmtz <josejavmartine26@gmail.com>
2020-04-23 21:20:53 -0700

    Only expose iOS vs App.ios.tsx

---
+++ App.ios.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Platform, View } from "react-native";
+import { useFonts } from "@use-expo/font";
+import { Ionicons, FontAwesome5 } from "@expo/vector-icons";
+import { AppLoading } from "expo";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { enableScreens } from "react-native-screens";
+import Navigation from "./Navigation";
+
+enableScreens();
+
+const CustomFonts = {
+  "NotoSans-Bold": require("./assets/fonts/NotoSans-Bold.ttf"),
+  "NotoSans-BoldItalic": require("./assets/fonts/NotoSans-BoldItalic.ttf"),
+  "NotoSans-Italic": require("./assets/fonts/NotoSans-Italic.ttf"),
+  "NotoSans-Regular": require("./assets/fonts/NotoSans-Regular.ttf"),
+  ...Ionicons.font,
+  ...FontAwesome5.font,
+};
+
+export default function App() {
+  const [isLoaded] = useFonts(CustomFonts);
+
+  if (!isLoaded) {
+    return <AppLoading />;
+  }
+
+  return (
+    <SafeAreaProvider>
+      <Navigation />
+    </SafeAreaProvider>
+  );
+}
--- App.tsx
+++ App.tsx
@@ -1,32 +1,10 @@
-import React from "react";
-import { useFonts } from "@use-expo/font";
-import { Ionicons, FontAwesome5 } from "@expo/vector-icons";
-import { AppLoading } from "expo";
-import { SafeAreaProvider } from "react-native-safe-area-context";
-import { enableScreens } from 'react-native-screens';
-import Navigation from "./Navigation";
-
-enableScreens();
-
-const CustomFonts = {
-  "NotoSans-Bold": require("./assets/fonts/NotoSans-Bold.ttf"),
-  "NotoSans-BoldItalic": require("./assets/fonts/NotoSans-BoldItalic.ttf"),
-  "NotoSans-Italic": require("./assets/fonts/NotoSans-Italic.ttf"),
-  "NotoSans-Regular": require("./assets/fonts/NotoSans-Regular.ttf"),
-  ...Ionicons.font,
-  ...FontAwesome5.font,
-};
+import * as React from "react";
+import { Text, View } from "react-native";
 
 export default function App() {
-  const [isLoaded] = useFonts(CustomFonts);
-
-  if (!isLoaded) {
-    return <AppLoading />;
-  }
-
   return (
-    <SafeAreaProvider>
-      <Navigation />
-    </SafeAreaProvider>
+    <View style={{ alignItems: "center", justifyContent: "center", flex: 1 }}>
+      <Text>This app has only been built for iOS, as per the original</Text>
+    </View>
   );
 }
